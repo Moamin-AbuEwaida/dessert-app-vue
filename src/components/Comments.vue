ef7382018c30bbe97dbeab06048e56da90863858
Moamin <moamin.salamah@gmail.com>
2022-09-21 14:06:19 +0200

    finished

---
+++ src/components/Comments.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row class="mt-n2">
+    <v-col cols="12" sm="1">
+      <v-avatar size="30" class="ml-4" title>
+        <v-img src="https://cdn.vuetifyjs.com/images/lists/4.jpg"></v-img>
+      </v-avatar>
+    </v-col>
+    <v-col cols="12" sm="11">
+      <v-rating
+        v-model="rating"
+        background-color="grey"
+        color="grey"
+        x-small
+      ></v-rating>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+        recusandae porro repellat in quo magni praesentium nostrum pariatur
+        autem repellendus.
+      </p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "CommentsPart",
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,34 @@
   <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
     <SideBar />
     <RightSideBar />
+    <v-container>
+      <Navbar />
+      <Desserts />
+      <v-toolbar flat color="transparent" class="mt-n4">
+        <v-toolbar-title class="ml-4">
+          <strong>Day</strong><span class="grey--text ml-2">Menu</span>
+        </v-toolbar-title>
+      </v-toolbar>
+      <Menu />
+      <v-toolbar flat color="transparent" class="mt-n4">
+        <v-toolbar-title class="ml-4">
+          <strong>Ingredients</strong><span class="grey--text ml-2">Food</span>
+        </v-toolbar-title>
+      </v-toolbar>
+      <Food />
+      <Comments />
+    </v-container>
   </v-app>
 </template>
 
 <script>
 import SideBar from "@/components/SideBar.vue";
 import RightSideBar from "@/components/RightSideBar.vue";
+import Navbar from "@/components/Navbar.vue";
+import Desserts from "../components/Desserts.vue";
+import Menu from "@/components/Menu.vue";
+import Comments from "../components/Comments.vue";
+import Food from "../components/Food.vue";
 
 export default {
   name: "HomeView",
@@ -15,6 +37,11 @@ export default {
   components: {
     SideBar,
     RightSideBar,
+    Navbar,
+    Desserts,
+    Menu,
+    Comments,
+    Food,
   },
 };
 </script>
